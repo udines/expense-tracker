32bfad04914459509313ac1fc22cb6398452072c
Fata Az <sendtofata@live.com>
2025-01-30 18:45:09 +0700

    Add domain entities

---
+++ app/src/main/java/com/udinesfata/expenz/domain/entity/Category.kt
@@ -0,0 +1,7 @@
+package com.udinesfata.expenz.domain.entity
+
+class Category(
+    val id: Int,
+    val name: String,
+    val type: String, // income or expense
+)
---
+++ app/src/main/java/com/udinesfata/expenz/domain/entity/Wallet.kt
@@ -0,0 +1,8 @@
+package com.udinesfata.expenz.domain.entity
+
+class Wallet(
+    val id: Int,
+    val name: String,
+    val balance: Double,
+    val currency: String,
+)
