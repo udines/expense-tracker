efbc8248295c4cf6c9869be9e6db6149fca9c448
Fata Az <sendtofata@live.com>
2025-01-31 14:51:04 +0700

    Add serialized name annotation to came-cased properties

--- app/src/main/java/com/udinesfata/expenz/data/model/remote/BudgetResponse.kt
+++ app/src/main/java/com/udinesfata/expenz/data/model/remote/BudgetResponse.kt
@@ -1,10 +1,16 @@
 package com.udinesfata.expenz.data.model.remote
 
+import com.google.gson.annotations.SerializedName
+
 data class BudgetResponse(
     val id: Int,
+    @SerializedName("category_id")
     val categoryId: Int,
     val amount: Double,
+    @SerializedName("start_date")
     val startDate: String, // ISO 8601 date format
+    @SerializedName("end_date")
     val endDate: String, // ISO 8601 date format
+    @SerializedName("wallet_id")
     val walletId: Int,
 )
--- app/src/main/java/com/udinesfata/expenz/data/model/remote/TransactionResponse.kt
+++ app/src/main/java/com/udinesfata/expenz/data/model/remote/TransactionResponse.kt
@@ -1,11 +1,15 @@
 package com.udinesfata.expenz.data.model.remote
 
+import com.google.gson.annotations.SerializedName
+
 class TransactionResponse(
     val id: Int,
     val amount: Double,
     val date: String, // ISO 8601 date format
     val notes: String,
+    @SerializedName("category_id")
     val categoryId: Int,
+    @SerializedName("wallet_id")
     val walletId: Int,
     val type: String, // income or expense
     val currency: String,
