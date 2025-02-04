fe64bd0663d3bf00c3526896b8049a645be59bf4
Fata Az <sendtofata@live.com>
2025-02-04 14:35:28 +0700

    Update repository to accept params argument

--- app/src/main/java/com/udinesfata/expenz/domain/repository/CategoryRepository.kt
+++ app/src/main/java/com/udinesfata/expenz/domain/repository/CategoryRepository.kt
@@ -1,10 +1,11 @@
 package com.udinesfata.expenz.domain.repository
 
 import com.udinesfata.expenz.domain.entity.Category
+import com.udinesfata.expenz.domain.params.CategoryParams
 
 interface CategoryRepository {
     suspend fun getCategory(id: Int, forceRefresh: Boolean = true): Category
-    suspend fun getCategories(): List<Category>
+    suspend fun getCategories(params: CategoryParams): List<Category>
     suspend fun createCategory(category: Category)
     suspend fun updateCategory(category: Category)
     suspend fun deleteCategory(id: Int)
