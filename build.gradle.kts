d00f860b6af99ff49e0080a3a8616566e971b676
Fata Az <sendtofata@live.com>
2025-02-14 20:16:15 +0700

    Add compose library to build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
